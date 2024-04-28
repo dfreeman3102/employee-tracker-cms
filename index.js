57984c300c9d52d95e7fbc070265bfa034989134
dfreeman3102 <davidfreeman3102@gmail.com>
2024-04-28 17:19:23 -0400

    got the first 3 choices to function properly, need to add function for the last 4 choices

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const inquirer = require("inquirer");
-const mysql = require("mysql2");
 const fs = require("fs");
+//adding mysql2
+const mysql = require("mysql2");
+const dotenv = require("dotenv").config();
+const { dbNAME, dbPASSWORD, dbUSER } = process.env;
 
+//creating initial connection
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    database: dbNAME,
+    password: dbPASSWORD,
+    user: dbUSER
+  },
+  console.log("Connected to the company_db!")
+);
 inquirer
-    .createPromptModule([
+    .prompt([
         {
             type: "list",
-            name: "list-choices",
+            name: "list",
             message: "Choose one of the following options.",
             choices:["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
         },
-    ]);
+    ])
+    .then((data) =>{
+        if(data.list === "View all departments"){
+            db.query("SELECT * FROM department", function (err, results) {
+                console.table(results);
+            });
+        }
+        if(data.list === "View all roles"){
+            db.query("SELECT * FROM role", function (err, results) {
+                console.table(results);
+            });
+        }
+        if(data.list === "View all employees"){
+            db.query("SELECT * FROM employee", function (err, results) {
+                console.table(results);
+            });
+        }
+    });
