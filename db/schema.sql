6500d49414f7cb0c7373c4615fd1f3af965cca91
dfreeman3102 <davidfreeman3102@gmail.com>
2024-04-28 18:15:27 -0400

    added auto increment to all the table IDs

--- db/schema.sql
+++ db/schema.sql
@@ -6,12 +6,12 @@ USE company_db;
 
 -- Creates the department table
 CREATE TABLE department(
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 -- Creates the role table
 CREATE TABLE role(
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
@@ -21,7 +21,7 @@ CREATE TABLE role(
 );
 -- CREATES the employee table
 CREATE TABLE employee(
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
